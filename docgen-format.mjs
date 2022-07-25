f488b811d6f9323ff8b9e1b4cc233deb1efa42da
Aparajita <aparajita@aparajita.com>
2022-07-24 17:52:35 -0700

    chore: leave index as is

--- docgen-format.mjs
+++ docgen-format.mjs
@@ -3,26 +3,6 @@ import fs from 'fs'
 
 const kGreenCheck = '\x1b[32m✔\x1b[0m'
 
-function indexFixer(match, index) {
-  // 1. Remove bullets
-  // 2. Remove code format from method names
-  // 2. Add extra \n before Interfaces
-  const fixedIndex = index
-    .replace(/^\* /gmu, '')
-    .replace(/\[`(.+)`\](.+)/gu, '[$1]$2')
-    .replace(/\[Interfaces\]/u, '\n[Interfaces]')
-
-  return `<docgen-index>${fixedIndex}</docgen-index>`
-}
-
-function fixIndex(content) {
-  console.log(`${kGreenCheck} Fixing index`)
-  return content.replace(
-    /^\s*<docgen-index>(.+)<\/docgen-index>/msu,
-    indexFixer
-  )
-}
-
 function tableHeaderFixer(match, dashes) {
   // Left align headers
   return `| :${dashes} `
@@ -44,7 +24,6 @@ function fixReturnTypes(content) {
 function main() {
   console.log('\x1b[1m\x1b[34mdocgen-format\x1b[0m')
   let content = fs.readFileSync('README.md', 'utf-8')
-  content = fixIndex(content)
   content = fixTables(content)
   content = fixReturnTypes(content)
   fs.writeFileSync('README.md', content, 'utf-8')
